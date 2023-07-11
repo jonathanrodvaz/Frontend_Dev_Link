34649378b791f2ad205be4d9e200fdfae8eaae99
jonathanrodvaz <jonathanrodvazquez@gmail.com>
2023-07-11 11:37:00 +0200

    Updated service.config

--- src/services/API_proyect/service.config.js
+++ src/services/API_proyect/service.config.js
@@ -10,8 +10,11 @@ const APIHeaders = {
 };
 
 export const API = axios.create({
-  baseURL: `https://backend-dev-link.vercel.app/api/v1`,
+  baseURL: `https://backend-dev-link-flax.vercel.app/api/v1`,
   // baseURL: `http://localhost:8080/api/v1`,
   headers: APIHeaders,
   timeout: 60000,
 });
+
+
+//Original baseURL in line 12: baseURL: `https://backend-dev-link.vercel.app/api/v1`,
